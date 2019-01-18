08d4ca2f6d94f0ef0e4499cd65318dd53a5b5377
marcialwushu <marcialwushu@gmail.com>
2019-01-18 12:04:14 -0200

    HttpPost SaveEvent and DeleteEvent in HomeController added

--- MVCEventCalendar/Controllers/HomeController.cs
+++ MVCEventCalendar/Controllers/HomeController.cs
@@ -9,13 +9,17 @@ namespace MVCEventCalendar.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        /// <summary>
+        ///   <para>GET : Home DataEvent for Calendar</para>
+        ///   <para>return View(); </para>
+        /// </summary>
         public ActionResult Index()
         {
             return View();
         }
 
         
-        /**
+        
         public JsonResult GetEvents()
         {
             using (MyDatabaseEntities dc = new MyDatabaseEntities())
@@ -24,6 +28,54 @@ namespace MVCEventCalendar.Controllers
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
         }
-        */
+
+        [HttpPost]
+        public JsonResult SaveEvent(Events e )
+        {
+            var status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+
+                dc.SaveChanges();
+                status = true;
+            }
+
+            return new JsonResult { Data = new {  status } };
+        }
+        
+        [HttpPost]
+        public JsonResult DeleteEvent(int EventID)
+        {
+            var status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Events.Where(a => a.EventID == EventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
 }
